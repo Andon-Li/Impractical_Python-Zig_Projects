caec594d22b870060775eb6cfc06e64a7c2461ac
Andon Li <andonli281@gmail.com>
2024-05-15 13:02:13 -0400

    idk

--- Chapter 1/Generating Pseudonyms/main.zig
+++ Chapter 1/Generating Pseudonyms/main.zig
@@ -6,7 +6,7 @@ pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
     const slice = try allocator.alloc(u8, 10);
-    defer allocator.free();
+    defer allocator.free(slice);
 
     try stdout.writeAll("Hello World!\n");
     const data = try stdin.readUntilDelimiterOrEof(slice, '\n') orelse return;
